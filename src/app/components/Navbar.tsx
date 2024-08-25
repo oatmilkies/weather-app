f795a35129cec068be650ff78b0997fcb2009c7b
Leilani <pquaby@gmail.com>
2024-08-25 14:35:48 -0400

    Add navbar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+/** @format */
+
+import React from 'react';
+
+type Props = {};
+
+export default function Navbar({ }: Props) {
+  return (
+    <nav className='shadow-sm sticky top-0 left-0 z-50 bg-white' >
+      <div className='h-[80px] w-full flex justify-between items-center max-w-7xl px-3'>
+        <div className='flex items-center justify-center gap-2'>
+          <h2 className='text-gray-500 text-3xl'>Weather</h2>
+        </div>
+      </div>
+      Navbar
+    </nav>
+  );
+}
